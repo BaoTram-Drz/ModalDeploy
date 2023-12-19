6f8ef23041a825f5b7a9b127e5d7f87e84f096e6
Đặng Bảo Trâm <90879088+BaoTram-Drz@users.noreply.github.com>
2023-12-19 11:33:20 +0700

    Update runModal.py

--- runModal.py
+++ runModal.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import h5py
 
 def read_hdf5_file(file_path):
@@ -6,15 +7,18 @@ def read_hdf5_file(file_path):
             # Đọc dữ liệu từ file HDF5 ở đây
             # Ví dụ: Đọc dataset có tên là 'data'
             data = file['data'][:]
-            
-            # Thực hiện các thao tác khác với dữ liệu
-            
-            print("Đọc file HDF5 thành công.")
             return data
     except Exception as e:
-        print(f"Lỗi khi đọc file HDF5: {str(e)}")
+        st.error(f"Lỗi khi đọc file HDF5: {str(e)}")
         return None
 
-if __name__ == "__main__":
-    file_path = "modal.hdf5"  # Đường dẫn đến file HDF5 của bạn
-    read_hdf5_file(file_path)
+# Đường dẫn đến file HDF5 của bạn
+file_path = "modal.hdf5"
+
+# Đọc dữ liệu từ file HDF5
+modal_data = read_hdf5_file(file_path)
+
+# Hiển thị dữ liệu bằng Streamlit
+if modal_data is not None:
+    st.write("Dữ liệu từ file modal.hdf5:")
+    st.write(modal_data)
