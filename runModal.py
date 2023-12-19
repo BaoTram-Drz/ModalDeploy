3d8bc62a3aefa1d2f8b2065faad4e71b59f307c3
BaoTram-Drz <90879088+BaoTram-Drz@users.noreply.github.com>
2023-12-19 22:32:59 +0700

    test

--- runModal.py
+++ runModal.py
@@ -1,29 +1,27 @@
 import streamlit as st
-
-try:
-    import h5py
-except Exception as e:
-    st.error(f"Lỗi khi import mô-đun h5py: {str(e)}")
-    st.exception(e)
+import h5py
 
 def read_hdf5_file(file_path):
     try:
         with h5py.File(file_path, 'r') as file:
-            # Đọc dữ liệu từ file HDF5 ở đây
-            # Ví dụ: Đọc dataset có tên là 'data'
+            # Read data from the HDF5 file here
             data = file['data'][:]
             return data
     except Exception as e:
-        st.error(f"Lỗi khi đọc file HDF5: {str(e)}")
+        st.error(f"Error reading HDF5 file: {str(e)}")
         return None
 
-# Đường dẫn đến file HDF5 của bạn
-file_path = "modal.hdf5"
+# Allow users to upload the HDF5 file
+uploaded_file = st.file_uploader("Upload HDF5 file", type=["hdf5", "h5"])
 
-# Đọc dữ liệu từ file HDF5
-modal_data = read_hdf5_file(file_path)
-
-# Hiển thị dữ liệu bằng Streamlit
-if modal_data is not None:
-    st.write("Dữ liệu từ file modal.hdf5:")
-    st.write(modal_data)
+# Display data if the file is uploaded
+if uploaded_file:
+    # Save the uploaded file to a temporary location
+    with open("temp.hdf5", "wb") as f:
+        f.write(uploaded_file.read())
+    # Read data from the HDF5 file
+    modal_data = read_hdf5_file("temp.hdf5")
+    # Display the data
+    if modal_data is not None:
+        st.write("Data from HDF5 file:")
+        st.write(modal_data)
