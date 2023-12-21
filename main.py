a6b41b77e395a452c047a5cf49e8891bb8dc97b5
Đặng Bảo Trâm <90879088+BaoTram-Drz@users.noreply.github.com>
2023-12-21 10:04:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,79 +1,46 @@
-# # main.py
-# import subprocess
-# import os
-# import os
-# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-# from model import create_model
-
-# import streamlit as st
-# import numpy as np
-# from tensorflow.keras.optimizers import Adam
- 
-# input_shape = (64, 64, 3)
-# num_classes = 16
-# model = create_model(input_shape, num_classes)
- 
-# for layer in model.layers:
-#     print(layer.name)
- 
-# model.compile(optimizer=Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
- 
-# script_directory = os.path.dirname(os.path.abspath(__file__))
- 
-# weights_file_path = os.path.join(script_directory, "model.hdf5")  # Cập nhật tên file
-# try:
-#     model.load_weights(weights_file_path, by_name=True)
-# except ValueError as e:
-#     st.error(f"Lỗi: {str(e)}")
-# except Exception as e:
-#     st.error(f"Lỗi: Đã xảy ra một lỗi không mong muốn. {e}") 
-# new_data = np.random.rand(10, *input_shape)
-# predictions = model.predict(new_data)
-import streamlit as st
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.preprocessing.text import tokenizer_from_json
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import streamlit as st
 
-vocab_size = 10000
-embedding_dim = 100
-max_sequence_length = 200
+# Đường dẫn tới tệp "model.hdf5"
+model_path = 'model.hdf5'
 
-# Hàm tải model
-@st.cache(allow_output_mutation=True)
-def load_model():
-    model = keras.Sequential([
-        keras.layers.Embedding(input_dim=vocab_size, output_dim=embedding_dim, input_length=max_sequence_length),
-        keras.layers.Conv1D(filters=128, kernel_size=5, activation='relu'),
-        keras.layers.GlobalMaxPooling1D(),
-        keras.layers.Dense(64, activation='relu'),
-        keras.layers.Dense(1, activation='sigmoid')
-    ])
-    model.load_weights("model.hdf5")
-    with open('tokenizer.json') as f:
-        data = tokenizer_from_json(f.read())
-        tokenizer = data['tokenizer']
-    return model, tokenizer
+# Thiết lập các thông số huấn luyện
+input_shape = (64, 64, 3)
+num_classes = 2
+batch_size = 32
+epochs = 20
 
-# Giao diện người dùng
-st.title("Ứng dụng dự đoán")
+# Tiền xử lý dữ liệu huấn luyện và xác thực
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+train_generator = train_datagen.flow_from_directory('training_data_directory',
+                                                    target_size=input_shape[:2],
+                                                    batch_size=batch_size,
+                                                    class_mode='categorical')
 
-# Thực hiện tải model và tokenizer
-model, tokenizer = load_model()
+# Tạo mô hình và tải trọng số từ tệp "model.hdf5"
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+# Thêm các lớp khác của mô hình của bạn ở đây
+# ...
 
-# Thêm các thành phần giao diện, ví dụ: ô nhập liệu
-input_text = st.text_input("Nhập văn bản:", "")
+# Tải trọng số từ tệp "model.hdf5"
+model.load_weights(model_path)
 
-# Kiểm tra khi người dùng nhấn nút "Dự đoán"
-if st.button("Dự đoán"):
-    # Tiền xử lý văn bản đầu vào
-    input_sequence = tokenizer.texts_to_sequences([input_text])
-    input_sequence = keras.preprocessing.sequence.pad_sequences(input_sequence, maxlen=max_sequence_length)
+# Biên dịch mô hình
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-    # Sử dụng model để dự đoán
-    prediction = model.predict(input_sequence)
+# Huấn luyện mô hình
+model.fit(train_generator,
+          steps_per_epoch=train_generator.samples // batch_size,
+          epochs=epochs)
 
-    # Hiển thị kết quả
-    if prediction > 0.5:
-        st.write("Positive")
-    else:
-        st.write("Negative")
+# Lưu trữ trọng số huấn luyện thành tệp "model.hdf5"
+model.save_weights(model_path)
+
+# Hiển thị thông báo hoàn thành
+st.write("Huấn luyện mô hình đã hoàn thành!")
