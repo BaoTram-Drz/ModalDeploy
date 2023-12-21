568d43bc25c1642bf6807924e6dd64696f325c92
Đặng Bảo Trâm <90879088+BaoTram-Drz@users.noreply.github.com>
2023-12-21 10:15:56 +0700

    Update main

--- main.py
+++ main.py
@@ -1,46 +1,27 @@
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import streamlit as st
+import tensorflow as tf
 
-# Đường dẫn tới tệp "model.hdf5"
-model_path = 'model.hdf5'
+def load_model():
+    # Load the model from the .hdf5 file
+    model = tf.keras.models.load_model('model.hdf5')
+    return model
 
-# Thiết lập các thông số huấn luyện
-input_shape = (64, 64, 3)
-num_classes = 2
-batch_size = 32
-epochs = 20
+def main():
+    # Load the model
+    model = load_model()
 
-# Tiền xử lý dữ liệu huấn luyện và xác thực
-train_datagen = ImageDataGenerator(rescale=1./255,
-                                   shear_range=0.2,
-                                   zoom_range=0.2,
-                                   horizontal_flip=True)
-train_generator = train_datagen.flow_from_directory('training_data_directory',
-                                                    target_size=input_shape[:2],
-                                                    batch_size=batch_size,
-                                                    class_mode='categorical')
+    # Create a file uploader
+    uploaded_file = st.file_uploader("Upload an image", type=['png', 'jpg'])
 
-# Tạo mô hình và tải trọng số từ tệp "model.hdf5"
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-# Thêm các lớp khác của mô hình của bạn ở đây
-# ...
+    if uploaded_file is not None:
+        # Preprocess the image
+        image = preprocess_image(uploaded_file)
 
-# Tải trọng số từ tệp "model.hdf5"
-model.load_weights(model_path)
+        # Make predictions
+        predictions = model.predict(image)
 
-# Biên dịch mô hình
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        # Display the predictions
+        st.write(predictions)
 
-# Huấn luyện mô hình
-model.fit(train_generator,
-          steps_per_epoch=train_generator.samples // batch_size,
-          epochs=epochs)
-
-# Lưu trữ trọng số huấn luyện thành tệp "model.hdf5"
-model.save_weights(model_path)
-
-# Hiển thị thông báo hoàn thành
-st.write("Huấn luyện mô hình đã hoàn thành!")
+if __name__ == '__main__':
+    main()
