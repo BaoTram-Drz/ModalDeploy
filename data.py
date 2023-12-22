f7bdc0e819eeb2c2ea4130954a4fa394f2feef1f
BaoTram-Drz <90879088+BaoTram-Drz@users.noreply.github.com>
2023-12-22 21:03:53 +0700

    delete file

--- data.py
+++
@@ -1,9 +0,0 @@
-# data.py
-import numpy as np
-import h5py
-
-def load_data(file_path):
-    # Load your data from file (e.g., CSV, images, etc.)
-    # Preprocess and return data and labels
-    return X_train, y_train, X_val, y_val
-
