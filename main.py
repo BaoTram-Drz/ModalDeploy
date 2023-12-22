2349d0aaf408aa23b9412692dd1d70b2217902ae
BaoTram-Drz <90879088+BaoTram-Drz@users.noreply.github.com>
2023-12-22 21:07:19 +0700

    change

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
+# Python program to demonstrate
+# HDF5 file
+
+import numpy as np
 import h5py
-filename = "file.hdf5"
 
-with h5py.File(filename, "r") as f:
-    # Print all root level object names (aka keys) 
-    # these can be group or dataset names 
-    print("Keys: %s" % f.keys())
-    # get first object name/key; may or may NOT be a group
-    a_group_key = list(f.keys())[0]
+# initializing a random numpy array
+arr = np.random.randn(1000)
 
-    # get the object type for a_group_key: usually group or dataset
-    print(type(f[a_group_key])) 
-
-    # If a_group_key is a group name, 
-    # this gets the object names in the group and returns as a list
-    data = list(f[a_group_key])
-
-    # If a_group_key is a dataset name, 
-    # this gets the dataset values and returns as a list
-    data = list(f[a_group_key])
-    # preferred methods to get dataset values:
-    ds_obj = f[a_group_key]      # returns as a h5py dataset object
-    ds_arr = f[a_group_key][()]
+# creating a file
+# with h5py.File('model.hdf5', 'w') as f: 
+# 	dset = f.create_dataset("default", data = arr)
+	
+# open the file as 'f'
+with h5py.File('model.hdf5', 'r') as f: 
+	data = f['default']
+	
+	# get the minimum value
+	print(min(data)) 
+	
+	# get the maximum value
+	print(max(data))
+	
+	# get the values ranging from index 0 to 15
+	print(data[:16])
+	
