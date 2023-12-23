f1e7b92c99b65989ba822e1e7365ea9136c7402c
BaoTram-Drz <90879088+BaoTram-Drz@users.noreply.github.com>
2023-12-23 14:45:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def predict_image(model, img_array):
     img_array = np.expand_dims(img_array, axis=0)
 
     # Dự đoán
-    predictions = model.dir(img_array)
+    predictions = model.class(img_array)
 
     return predictions
 
